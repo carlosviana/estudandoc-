0d263ba0c69cd3ff6abed4bf77316ae18dc42126
carlosviana <viana.mail@gmail.com>
2015-03-07 21:15:32 -0300

    Editor de textos

--- Aprendendo/Aprendendo/Form1.Designer.cs
+++ Aprendendo/Aprendendo/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Aprendendo
 {
-    partial class Form1
+    partial class EditorDeTexto
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.textoConteudo = new System.Windows.Forms.TextBox();
+            this.botaoGrava = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textoConteudo
+            // 
+            this.textoConteudo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textoConteudo.Location = new System.Drawing.Point(21, 12);
+            this.textoConteudo.Multiline = true;
+            this.textoConteudo.Name = "textoConteudo";
+            this.textoConteudo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textoConteudo.Size = new System.Drawing.Size(384, 297);
+            this.textoConteudo.TabIndex = 0;
+            this.textoConteudo.TextChanged += new System.EventHandler(this.textoConteudo_TextChanged);
+            // 
+            // botaoGrava
+            // 
+            this.botaoGrava.Location = new System.Drawing.Point(181, 315);
+            this.botaoGrava.Name = "botaoGrava";
+            this.botaoGrava.Size = new System.Drawing.Size(75, 23);
+            this.botaoGrava.TabIndex = 1;
+            this.botaoGrava.Text = "Salva";
+            this.botaoGrava.UseVisualStyleBackColor = true;
+            this.botaoGrava.Click += new System.EventHandler(this.botaoGrava_Click);
+            // 
+            // EditorDeTexto
+            // 
+            this.ClientSize = new System.Drawing.Size(417, 350);
+            this.Controls.Add(this.botaoGrava);
+            this.Controls.Add(this.textoConteudo);
+            this.Name = "EditorDeTexto";
+            this.Load += new System.EventHandler(this.EditorDeTexto_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textoConteudo;
+        private System.Windows.Forms.Button botaoGrava;
     }
 }
 
--- Aprendendo/Aprendendo/Form1.cs
+++ Aprendendo/Aprendendo/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,39 @@ using System.Windows.Forms;
 
 namespace Aprendendo
 {
-    public partial class Form1 : Form
+    public partial class EditorDeTexto : Form
     {
-        public Form1()
+        public EditorDeTexto()
         {
             InitializeComponent();
         }
+
+        private void textoConteudo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EditorDeTexto_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("D:\\teste.log"))
+            {
+                using (Stream entrada = File.Open("D:\\teste.log", FileMode.Open))
+                using (StreamReader leitor = new StreamReader(entrada))
+                {
+                    string linha = leitor.ReadToEnd();
+                    textoConteudo.Text += linha;
+                }
+            }
+        }
+
+        private void botaoGrava_Click(object sender, EventArgs e)
+        {
+            using(Stream saida = File.Open("D:\\teste.log",FileMode.Create))
+            using (StreamWriter escritor = new StreamWriter(saida))
+            {
+                escritor.Write(textoConteudo.Text);
+            }
+        }
+
     }
 }
